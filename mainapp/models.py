ef37756c65eddee8ecf90c4e953c7756a1ac04a4
EA <pak_ea@inbox.ru>
2021-11-25 15:48:57 +0300

    updated models.py and migrations

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name='name', max_length=64, unique=True)
+    data = models.CharField(verbose_name='sign for css', max_length=25, default='*', blank=True)
+    description = models.TextField(verbose_name='description', blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'product category'
+        verbose_name_plural = 'product categories'
+        ordering = ['id']
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='product name', max_length=128)
+    image = models.ImageField(upload_to='products_images', blank=True)
+    description = models.TextField(verbose_name='product description', blank=True)
+    price = models.DecimalField(verbose_name='product price', max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name='quantity in stock', default=0)
+
+    def __str__(self):
+        return f'{self.name} ({self.category.name})'
+
+    class Meta:
+        ordering = ['category_id']
