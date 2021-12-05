e3b585e679aa8e5ceea943a0eb18560fdbc532b9
EA <pak_ea@inbox.ru>
2021-12-05 14:55:10 +0300

    added path to basketapp

--- clothingstore/urls.py
+++ clothingstore/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
 
     path('', include('mainapp.urls', namespace='main')),
     path('auth/', include('authapp.urls', namespace='auth')),
+    path('basket/', include('basketapp.urls', namespace='basket')),
 ]
 
 if settings.DEBUG:
