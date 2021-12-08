afc2aadb861e66b4d6fc99a1e24552a49aa4a213
EA <pak_ea@inbox.ru>
2021-12-08 15:44:03 +0300

    created function add

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import time
 
-# Create your views here.
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from basketapp.models import Basket
+
+
+def index(request):
+    pass
+
+
+def add(request, product_pk):
+    basket_item, _ = Basket.objects.get_or_create(
+        user=request.user,
+        product_id=product_pk
+    )
+    basket_item.quantity += 1
+    basket_item.save()
+
+    time.sleep(1)
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+def remove(request, pk):
+    pass
