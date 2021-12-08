5004278f50172f29b49dcfbf828d3786531d2c5f
EA <pak_ea@inbox.ru>
2021-12-08 15:45:56 +0300

    added path for dynamic output products category & product detail

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path('blog/', mainapp.blog, name='blog'),
     path('blog_detail/', mainapp.blog_detail, name='blog_detail'),
     path('product_detail/', mainapp.product_detail, name='product_detail'),
+    path('product_detail/<int:pk>/', mainapp.product_detail, name='product_detail'),
     path('shoping_cart/', mainapp.shoping_cart, name='shoping_cart'),
+    path('category/<int:pk>/', mainapp.category, name='category'),
 ]
