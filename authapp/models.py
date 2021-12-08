3c84c4c04aa38587cf234f768a761e63d05eb28b
EA <pak_ea@inbox.ru>
2021-12-08 17:36:32 +0300

    created basket_price & basket_quantity in ShopUser model

--- authapp/models.py
+++ authapp/models.py
@@ -6,3 +6,9 @@ from django.contrib.auth.models import AbstractUser
 class ShopUser(AbstractUser):
     age = models.PositiveIntegerField(null=True)
     avatar = models.ImageField(upload_to='avatars', blank=True)
+
+    def basket_price(self):
+        return sum(el.product.price for el in self.basket.all())
+
+    def basket_quantity(self):
+        return sum(el.quantity for el in self.basket.all())
