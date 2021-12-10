64862bed2cb71441ef845cb18eb9a8e14ab15d16
EA <pak_ea@inbox.ru>
2021-12-10 18:51:22 +0300

    added asynchronous editing of the number of products in the cart using AJAX

---
+++ static/js/basket_script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+window.onload = function() {
+    console.log('DOM ready');
+    $('.table_row').on('change', "input[type='number']", function (event) {
+        let quantity = event.target.value;
+        let basketItemPk = event.target.name;
+        console.log(basketItemPk, quantity);
+
+        $.ajax({
+            url: "/basket/update/" + basketItemPk + "/" + quantity + "/",
+
+            success: function (data) {
+            console.log(data);
+                $('.basket_summary').html(data.basket_summary);
+            },
+        });
+       // send to backend
+       // get from backend
+       // do smth in DOM
+    });
+}
