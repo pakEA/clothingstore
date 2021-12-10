27a50ebde7fcc82ed0326300643969c507a853b0
EA <pak_ea@inbox.ru>
2021-12-10 18:42:17 +0300

    small fix

--- authapp/models.py
+++ authapp/models.py
@@ -8,7 +8,7 @@ class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='avatars', blank=True)
 
     def basket_price(self):
-        return sum(el.product.price for el in self.basket.all())
+        return sum(el.product_cost for el in self.basket.all())
 
     def basket_quantity(self):
         return sum(el.quantity for el in self.basket.all())
