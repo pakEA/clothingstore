13e1ec120c5da2bf782abe13cbe8ce0e7531de29
EA <pak_ea@inbox.ru>
2021-12-17 13:55:44 +0300

    added forms for working with categories and products as admin

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
+from django.forms import ModelForm, HiddenInput
 
 from authapp.forms import ShopUserEditForm
+from mainapp.models import ProductCategory, Product
 
 
 class AdminShopUserUpdateForm(ShopUserEditForm):
@@ -9,3 +11,27 @@ class AdminShopUserUpdateForm(ShopUserEditForm):
         fields = ('username', 'first_name', 'last_name', 'password',
                   'email', 'age', 'avatar',
                   'is_staff', 'is_superuser', 'is_active')
+
+
+class AdminProductCategoryCreateForm(ModelForm):
+    class Meta:
+        model = ProductCategory
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = f'form-control {field_name}'
+
+
+class AdminProductUpdateForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = f'form-control'
+            if field_name == 'category':
+                field.widget = HiddenInput()
