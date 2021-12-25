eb0a5e70fab9483d8e6306ca79887c1044d1a88e
EA <pak_ea@inbox.ru>
2021-12-25 18:55:23 +0300

    updated

--- mainapp/models.py
+++ mainapp/models.py
@@ -36,10 +36,3 @@ class Product(models.Model):
 
     class Meta:
         ordering = ['category_id']
-
-
-class ProductDetail(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.product.name
